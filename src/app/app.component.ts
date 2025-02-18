48f005f2e1baf1abe512fc247126a78253fe227c
vijaysinghshekhawat <vijayshekhawat177@gmail.com>
2025-02-18 15:55:57 +0530

    Header Creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PostCreateComponent } from "./posts/post-create.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [PostCreateComponent],
+  imports: [PostCreateComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
